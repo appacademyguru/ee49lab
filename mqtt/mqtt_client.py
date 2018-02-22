8adf2eb9cdedeb10589419eab132dd35ee960e8c
appacademyguru <appacademy.megan@gmail.com>
2018-02-22 00:22:26 -0800

    commit: mqtt/mqtt_client not working error: *** Syntax: ('exception', b'Connecting to broker iot.eclipse.org ...\r\n', b'Traceback (most recent call last):\r\n  File "<stdin>", line 24, in <module>\r\n  File "mqttclient.py", line 64, in connect\r\nOSError: 23\r\n')

--- mqtt/mqtt_client.py
+++ mqtt/mqtt_client.py
@@ -1,4 +1,4 @@
-from mqttclient import MQTTClient
+from mqttclient import *
 from time import sleep
 #Connect
 # BROKER = "iot.eecs.berkeley.edu"
@@ -10,25 +10,28 @@ broker = str(BROKER)
 USER = ""
 PWD = ""
 
-print("Connecting to broker", BROKER, "...")
-mqtt = MQTTClient(BROKER)
+print("Connecting to broker", "iot.eclipse.org", "...") #this is where it stops working
+mqtt = MQTTClient("iot.eclipse.org")
+#that's the error i keep getting
+# *** Syntax: ('exception', b'Connecting to broker iot.eclipse.org ...\r\n', b'Traceback (most recent call last):\r\n  File "<stdin>", line 24, in <module>\r\n  File "mqttclient.py", line 64, in connect\r\nOSError: 23\r\n')
+
 # mqtt = MQTTClient(BROKER, user=USER, password=PWD, ssl=True)
 # client = mqtt.Client(USER)
-
 def mqtt_callback(topic, msg):
     print("RECEIVE topic = {}, msg = {}".format(topic, msg))
 
 # client.on_message = mqqt_callback
 # client.connect(BROKER)
-mqtt.set_callback(mqtt_callback)
-mqtt.connect(broker)
+#mqtt.set_callback(mqtt_callback) # the error is on this line but it's in one of the mqttclient files
+mqtt.connect("iot.eclipse.org")
 print("Connected!")
-mqtt.subscribe("guru/esp32")
+
 # mqtt.subscribe("iot49/b")
 #Publish-subscribe loop
 for i in range(100):
-    topic = "guru/esp32"
+    topic = "iot49/esp32"
     message = "hello " + str(i)
+    mqtt.subscribe("iot49/esp32", mqtt_callback)
     print("PUBLISH topic = {} message = {}".format(topic, message))
     mqtt.publish(topic, message)
     for _ in range(10):
